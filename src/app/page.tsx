1a4e276ec3ddb706e6332d57cd7be2546923cef6
cc1015 <125077096+cc1015@users.noreply.github.com>
2024-12-09 18:16:12 -0500

    update info page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function base() {
   return (
-    <main className="flex items-center space-x-4 mx-8 sm:mx-16">
-      <figure className="flex flex-col items-center sm:items-start">
-        <img src='/bumblebee.jpg' alt="personal photo" className="w-100 h-100 object-contain"
+    <main className="flex space-x-8 mx-8 sm:mx-16 h-screen">
+      <figure className="flex flex-col items-center items-start w-1/2 top-0">
+        <img src='/bumblebee.jpg' alt="dali painting"
         />
         <figcaption className="mt-2 text-sm italic ">
           Dream Caused by the Flight of a Bee Around a Pomegranate a Second Before Awakening, Dali
@@ -11,7 +11,7 @@ export default function base() {
           Credit: © 2009 Salvador Dalí, Gala-Salvador Dalí Foundation / Artists Rights Society (ARS), New York, Courtesy of Art Resource, NY/Photo by Erich Lessing
         </figcaption>
       </figure>
-      <div>
+      <div className="w-1/2">
         <p className="text-justify">Hi, my name is Curie.</p>
         <br />
         <p className="text-justify">
