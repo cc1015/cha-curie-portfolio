380b6cdeae9f8f5ab606d80c541615fd09ab56c4
cc1015 <125077096+cc1015@users.noreply.github.com>
2025-02-07 15:31:44 -0500

    photo descs

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -1,37 +1,36 @@
-"use client";
-import Image from 'next/image'
+import PhotoRow from "../components/PhotoRow";
 
 export default function Base() {
-  const images = [
-    "/DSC_0046.jpg",
-    "/DSC_0047.jpg",
-    "/DSC_0048.jpg",
-    "/DSC_0323.jpg",
-    "/CurieChaGraceBowersDSC_0229.jpg",
-    "/DSC_0223.jpg",
-    "/DSC_0352.jpg",
-    "/DSC_0565.jpg",
-    "/DSC_0650.jpg",
-    "/DSC_0561.jpg",
-    "/DSC_0782.jpg",
-    "/DSC_0823.jpg",
-  ];
+  const row_1 = {
+    images: ["/DSC_0046.jpg", "/DSC_0047.jpg", "/DSC_0048.jpg"],
+    desc: "costa rica",
+  };
+
+  const row_2 = {
+    images: [
+      "/DSC_0323.jpg",
+      "/CurieChaGraceBowersDSC_0229.jpg",
+      "/DSC_0223.jpg",
+    ],
+    desc: "levitate music festival // grace bowers shredding",
+  };
+
+  const row_3 = {
+    images: ["/DSC_0352.jpg", "/DSC_0565.jpg", "/DSC_0650.jpg"],
+    desc: "maris // megan thee stallion // leon bridges",
+  };
+
+  const row_4 = {
+    images: ["/DSC_0561.jpg", "/DSC_0782.jpg", "/DSC_0823.jpg"],
+    desc: "khruangbin!!",
+  };
 
   return (
-    <main className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-3 gap-2 sm:mx-0">
-      {images.map((src, index) => (
-        <div key={index} className="w-full h-auto">
-          <Image
-            key={index}
-            src={src}
-            alt={`photo-${index + 1}`}
-            className="object-contain"
-            layout="intrinsic"
-            width={500} 
-            height={300}
-          />
-        </div>
-      ))}
+    <main>
+      <PhotoRow images={row_1.images} desc={row_1.desc}></PhotoRow>
+      <PhotoRow images={row_2.images} desc={row_2.desc}></PhotoRow>
+      <PhotoRow images={row_3.images} desc={row_3.desc}></PhotoRow>
+      <PhotoRow images={row_4.images} desc={row_4.desc}></PhotoRow>
     </main>
   );
 }
